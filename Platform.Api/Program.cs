8ca2a57260ce7294484086d40bbea43f9b6ed144
MOI <MOI@DESKTOP-8U076I2>
2025-12-22 11:48:54 +0300

    Fixed Assets and Documents Retrieval Bug

--- Platform.Api/Program.cs
+++ Platform.Api/Program.cs
@@ -64,7 +64,11 @@ builder.Services.AddCors(options =>
 });
 #endregion
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 
 var app = builder.Build();
 
