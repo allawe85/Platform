fed868952dfbe65b842671903801993286ac9886
MOI <MOI@DESKTOP-8U076I2>
2025-12-31 11:42:20 +0300

    Fixing Announcements and new DB BKP

--- Platform.Data/DTOs/Announcement.cs
+++ Platform.Data/DTOs/Announcement.cs
@@ -1,22 +1,34 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Platform.Data.DTOs
 {
     public class Announcement
     {
         [Key]
-        public int AnnouncementId { get; set; }
+        [Column("id")]
+        public int Id { get; set; }
 
         [Required]
         [MaxLength(200)]
         public string Title { get; set; } = string.Empty;
 
         [Required]
+        [MaxLength(200)]
+        [Column("title_ar")]
+        public string TitleAr { get; set; } = string.Empty;
+
+        [Required]
         public string Description { get; set; } = string.Empty;
 
+        [Column("description_ar")]
+        public string? DescriptionAr { get; set; }
+
+        [Column("created_date")]
         public DateTime CreatedDate { get; set; }
 
+        [Column("is_active")]
         public bool IsActive { get; set; }
     }
 }
