a55e20b79528421be7a5d05d0e570ac719ab5509
Alaa Bassam Rasheed <arasheed@burgan.com>
2026-01-02 12:38:02 +0300

    Employee Home Refactoring

--- Platform.Blazor/Services/LayoutService.cs
+++ Platform.Blazor/Services/LayoutService.cs
@@ -4,7 +4,18 @@ namespace Platform.Blazor.Services
 {
     public class LayoutService
     {
-        public MudTheme CurrentTheme { get; private set; } = new MudTheme();
+        public MudTheme CurrentTheme { get; private set; } = new MudTheme()
+        {
+            PaletteLight = new PaletteLight()
+            {
+                Primary = "#0D1E4C",
+                AppbarBackground = "#0D1E4C"
+            },
+            PaletteDark = new PaletteDark()
+            {
+                Primary = "#0D1E4C"
+            }
+        };
         public bool IsDarkMode { get; private set; }
         public event Action OnMajorUpdate;
 
